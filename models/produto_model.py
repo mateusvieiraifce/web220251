a6bc96b3d4ef2f08dbeaa5d1d051faecece89ab7
Luangtam <lf631255@gmail.com>
2025-08-07 10:29:32 -0300

    Luan

--- models/produto_model.py
+++ models/produto_model.py
@@ -3,13 +3,14 @@
 from sqlalchemy import Column, Integer, String, Float
 
 from models.Conexao import Base, engine
-
+from sqlalchemy.orm import relationship
 
 class Produto(Base):
     __tablename__ = 'produtos'
     id = Column(Integer, primary_key=True, autoincrement=True)
     nome = Column(String, nullable=False)
     preco = Column(Float, nullable=False)
+    vendas = relationship("Venda", back_populates="produto")
     def __init__(self, nome, preco):
         self.nome = nome
         self.preco = preco
